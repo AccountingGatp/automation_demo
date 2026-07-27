fd8987c13812c0aa10599e6bc57f213fdb921a2f
DIWAKARKASHYAP <didk9675@gmail.com>
2026-07-27 15:37:27 +0530

    fix cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,13 +5,7 @@ const apiRoutes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
-
-app.use(
-  cors({
-    origin: FRONTEND_URL,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.get('/health', (_req, res) => {
